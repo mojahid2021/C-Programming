facf882f740d4eb785c454f18ea38a8941136742
Md Mojahid <aammojahid@gmail.com>
2024-12-01 03:51:59 +0600

    Multiplication function

--- multiplication.c
+++ multiplication.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main () {
-    int a, b, addition;
+    int a, b, multiplication;
     printf("Enter the 1st Value: ");
     scanf("%d",&a);
     printf("Enter the 2hd Value: ");
     scanf("%d",&b);
 
-    addition = a * b;
-    printf("The result of Multiplication is : %d\n", addition);
+    multiplication = a * b;
+    printf("The result of Multiplication is : %d\n", multiplication);
 
     return 0;
 }
