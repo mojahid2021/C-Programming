1e73a221678d9ded5f2137438a375135e7b99016
Md Mojahid <aammojahid@gmail.com>
2024-12-01 03:43:19 +0600

    addition function

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+    int a, b, addition;
+    printf("Enter the 1st Value: ");
+    scanf("%d",&a);
+    printf("Enter the 2hd Value: ");
+    scanf("%d",&b);
+
+    addition = a + b;
+    printf("The result of addition is : %d", addition);
+
+    return 0;
+}
