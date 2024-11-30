8569a7ddbcb72917e2c03680954357848a821f0e
Md Mojahid <aammojahid@gmail.com>
2024-12-01 03:46:07 +0600

    get integer input

--- Input_integer.c
+++ Input_integer.c
@@ -3,7 +3,7 @@ int main () {
     int a;
     printf("Enter the integer value :");
     scanf("%d", &a); // scanf funtion uses for get input from user. 
-    printf("Here is the value you entered: %d", a);
+    printf("Here is the value you entered: %d\n", a);
 
     return 0;
 }
