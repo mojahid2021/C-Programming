2ec300b9ce2e31fd83892847f931c07f2da5db6d
Md Mojahid <aammojahid@gmail.com>
2024-12-01 04:16:58 +0600

    Print using for loop

---
+++ for_loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main () {
+    int i = 0 , n = 5; // if print 1 to 5 , just change the i value is = 1
+    for ( i ; i < n; i++) // if print 1 to 5, just add i <= n
+    {
+        printf("%d - Hello for loop! \n", i);
+    }
+    
+    
+    return 0;
+}
